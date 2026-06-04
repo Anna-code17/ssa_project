08c61d362cef143496a7cd041cf565ac0687ac68
Sara-Are <sara.areski@studenti.unipd.it>
2026-06-04 17:51:01 +0200

    Create EnvironmentalTaxPolicy.java

---
+++ proj/src/main/java/EnvironmentalTaxPolicy.java
@@ -0,0 +1,30 @@
+public class EnvironmentalTaxPolicy implements Policy {
+
+/* EnvironmentalTaxPolicy aumenta del 10% la felicità, diminuisce del 15% l'inquinamento
+ e il budget*/
+    
+    @Override
+    public String getName() {
+        return "Environmental Tax Policy";
+    }
+    
+    @Override
+    public int getPercentBudget() {
+        return -15;
+    }
+    
+    @Override
+    public int getPercentPopulation() {
+        return 0;
+    }
+    
+    @Override
+    public int getPercentPollution() {
+        return -15;
+    }
+    
+    @Override
+    public int getPercentHappiness() {
+        return 10;
+    }
+}
