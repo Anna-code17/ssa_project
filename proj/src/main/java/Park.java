2261755db3b01cb7550c9a34f259c749e1871bdb
Anna-code17 <anna.sattin.1@studenti.unipd.it>
2026-06-10 11:27:49 +0200

    Update Park.java

--- proj/src/main/java/Park.java
+++ proj/src/main/java/Park.java
@@ -2,7 +2,10 @@ public class Park extends Infrastructure {
 
     public Park() {
         super("Park");
-        this.effects = EffectLoader.load("park.json");
+        this.effects = JsonManager.loadFromResources(
+        "effects/park.json",
+        Effect.class
+        );
     }
     //metodo per stampare l'entita' sulla griglia
     @Override
