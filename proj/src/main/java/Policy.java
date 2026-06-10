1062815e0e296dd67af235c2e9c85acc85c6a568
Anna-code17 <anna.sattin.1@studenti.unipd.it>
2026-06-10 11:29:48 +0200

    Update Policy.java

--- proj/src/main/java/Policy.java
+++ proj/src/main/java/Policy.java
@@ -1,3 +1,17 @@
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+
+
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "type"
+)
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = EnvironmentalTaxPolicy.class, name = "EnvironmentalTaxPolicy"),
+    @JsonSubTypes.Type(value = IndustrialExpansionPolicy.class, name = "IndustrialExpansionPolicy")
+})
+
 public interface Policy {
     
     String getName();
