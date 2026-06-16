018915f3eca78ea0c265a40fa492644f3ad7f96b
Anna-code17 <anna.sattin.1@studenti.unipd.it>
2026-06-16 12:37:09 +0200

    Update City.java

--- proj/src/main/java/City.java
+++ proj/src/main/java/City.java
@@ -1,6 +1,5 @@
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
-import java.io.*;
 
 @JsonAutoDetect(
     fieldVisibility = JsonAutoDetect.Visibility.ANY,
